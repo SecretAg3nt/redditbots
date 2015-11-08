530697d308cd93d1315d9b4a8c3ad08df3958274
SecretAg3nt <hawkinsliam@hotmail.com>
2015-11-08 14:59:18 -0500

    Adding pelicanbot.py

---
+++ pelicanbot/pelicanbot.py
@@ -0,0 +1,38 @@
+import praw
+from time import ctime, sleep, time, strftime
+import obot
+
+
+commDone = set()
+subDone = set()
+NUMCOMMENTS = 0
+SETPHRASES = ["testpassword"]
+COMMLIMIT = 100
+
+print(strftime("%Y-%m-%d %H:%M:%S"))
+
+r = obot.login()
+print("Logged in")
+while True:
+  try:
+    for comment in praw.helpers.comment_stream(r, 'all', limit=COMMLIMIT, verbosity=0):
+          if comment.id not in commDone:
+            cbody = comment.body.lower()
+            cwords = cbody.split()
+            commDone.add(comment.id)
+            for word in cwords:
+              for key in SETPHRASES:
+                if word == key:
+                  NUMCOMMENTS = NUMCOMMENTS + 1
+                  msg = str(NUMCOMMENTS) + ") *" + strftime("%Y-%m-%d %H:%M:%S") + "* Someone is talking about \"" + key + "\": " + comment.author.name + " " + comment.permalink
+                  print(msg)
+                  subject = "Someone is talking about \"" + key
+                  r.send_message('SecretAg3nt', subject, msg)
+                  print("Message Sent")
+                  if comment.submission.id not in subDone:
+                    comment.reply("This is a test reply")
+                    print("Replied to comment")
+                    subDone.add(comment.submission.id)
+
+  except:
+    pass
