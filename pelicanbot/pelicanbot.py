39c90a602885497025c713b0e04d16753bc69a24
SecretAg3nt <hawkinsliam@hotmail.com>
2015-11-08 15:09:37 -0500

    Added proper reply message

--- pelicanbot/pelicanbot.py
+++ pelicanbot/pelicanbot.py
@@ -1,12 +1,12 @@
 import praw
-from time import ctime, sleep, time, strftime
+from time import strftime
 import obot
 
 
 commDone = set()
 subDone = set()
 NUMCOMMENTS = 0
-SETPHRASES = ["testpassword"]
+SETPHRASES = ["pelican"]
 COMMLIMIT = 100
 
 print(strftime("%Y-%m-%d %H:%M:%S"))
@@ -14,25 +14,25 @@ print(strftime("%Y-%m-%d %H:%M:%S"))
 r = obot.login()
 print("Logged in")
 while True:
-  try:
-    for comment in praw.helpers.comment_stream(r, 'all', limit=COMMLIMIT, verbosity=0):
-          if comment.id not in commDone:
-            cbody = comment.body.lower()
+    try:
+        for comment in praw.helpers.comment_stream(r, 'all', limit=COMMLIMIT, verbosity=0):
+            if comment.id not in commDone:
+                cbody = comment.body.lower()
             cwords = cbody.split()
             commDone.add(comment.id)
             for word in cwords:
-              for key in SETPHRASES:
-                if word == key:
-                  NUMCOMMENTS = NUMCOMMENTS + 1
-                  msg = str(NUMCOMMENTS) + ") *" + strftime("%Y-%m-%d %H:%M:%S") + "* Someone is talking about \"" + key + "\": " + comment.author.name + " " + comment.permalink
-                  print(msg)
-                  subject = "Someone is talking about \"" + key
-                  r.send_message('SecretAg3nt', subject, msg)
-                  print("Message Sent")
-                  if comment.submission.id not in subDone:
-                    comment.reply("This is a test reply")
-                    print("Replied to comment")
-                    subDone.add(comment.submission.id)
+                for key in SETPHRASES:
+                    if word == key:
+                        NUMCOMMENTS = NUMCOMMENTS + 1
+                        msg = str(NUMCOMMENTS) + ") *" + strftime("%Y-%m-%d %H:%M:%S") + "* Someone is talking about \"" + key + "\": " + comment.author.name + " " + comment.permalink
+                        print(msg)
+                        subject = "Someone is talking about \"" + key
+                        r.send_message('SecretAg3nt', subject, msg)
+                        print("Message Sent")
+                        if comment.submission.id not in subDone:
+                            comment.reply("Don't fuck with [pelicans](https://www.youtube.com/watch?v=jWxIOdt-V8Y)")
+                            print("Replied to comment")
+                            subDone.add(comment.submission.id)
 
-  except:
-    pass
+    except:
+        pass
