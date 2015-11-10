1b28e27470d9604616f06fc701b55d0d28ed8ccb
SecretAg3nt <hawkinsliam@hotmail.com>
2015-11-10 07:02:54 -0500

    Added subreddit blacklist to prevent spamming basketball subreddits

--- pelicanbot/pelicanbot.py
+++ pelicanbot/pelicanbot.py
@@ -5,6 +5,7 @@ from time import strftime
 
 NUMCOMMENTS = 0
 SETPHRASES = ["pelican", "pelicans"]
+SUBBLACKLIST = ["nba"]
 COMMLIMIT = 100
 COMMREPLY = "Don't fuck with the [pelicans](https://www.youtube.com/watch?v=jWxIOdt-V8Y)"
 USERNAME = "SecretAg3nt"
@@ -26,21 +27,23 @@ while True:
     try:
         for comment in praw.helpers.comment_stream(r, 'all', limit=COMMLIMIT, verbosity=0):
             if comment.id not in commDone:
-                cbody = comment.body.lower()
-            cwords = cbody.split()
-            commDone.add(comment.id)
-            for word in cwords:
-                for key in SETPHRASES:
-                    if word == key and comment.subreddit.display_name != "nba":
-                        NUMCOMMENTS = NUMCOMMENTS + 1
-                        print(create_message(NUMCOMMENTS, key, comment))
-                        subject = "Someone is talking about " + key
-                        r.send_message(USERNAME, subject, create_message(NUMCOMMENTS, key, comment))
-                        print("Message Sent to " + USERNAME)
-                        if comment.submission.id not in subDone:
-                            comment.reply(COMMREPLY)
-                            print("Replied to comment")
-                            subDone.add(comment.submission.id)
+                commDone.add(comment.id)
+                for word in comment.body.lower().split():
+                    for key in SETPHRASES:
+                        if word == key:
+                            NUMCOMMENTS = NUMCOMMENTS + 1
+                            print(create_message(NUMCOMMENTS, key, comment))
+                            subject = "Someone is talking about " + key
+                            r.send_message(USERNAME, subject, create_message(NUMCOMMENTS, key, comment))
+                            print("Message Sent to " + USERNAME)
+                            
+                            if comment.submission.id not in subDone and not comment.subreddit.display_name in SUBBLACKLIST:
+                                comment.reply(COMMREPLY)
+                                print("Replied to comment")
+                                subDone.add(comment.submission.id)
+                            else:
+                                print("Reply not sent, subreddit black-listed.")
+                                subDone.add(comment.submission.id)
 
     except:
         pass
