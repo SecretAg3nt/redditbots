2ebbd06d08648d1d1d7f7ef30ddaadfe5284782a
SecretAg3nt <hawkinsliam@hotmail.com>
2015-11-10 14:04:36 -0500

    Changed create_message and send_update to be specific to PelicanBot

--- pelicanbot/pelicanbot.py
+++ pelicanbot/pelicanbot.py
@@ -3,32 +3,29 @@ import obot
 from time import strftime
 
 
-NUMCOMMENTS = 0
-SETPHRASES = ["pelican", "pelicans"]
+KEYPHRASES = ["pelican", "pelicans"]
 SUBBLACKLIST = ["nba", "test"]
 COMMLIMIT = 100
 COMMREPLY = "Don't fuck with the [pelicans](https://www.youtube.com/watch?v=jWxIOdt-V8Y)"
 USERNAME = "SecretAg3nt"
 
-commDone = set()
-subDone = set()
-
 
 # Creates msg from keyword and comment passed in
-def create_message(NUMCOMMENTS, key, comment):
-    return str(NUMCOMMENTS) + ") " + strftime("%Y-%m-%d %H:%M:%S") + " Someone is talking about \"" + key + "\": " + comment.author.name + " " + comment.permalink
+def create_message(comment):
+    return "PelicanBot found someone talking about pelicans! " + comment.author.name + " " + comment.permalink
 
 
-def send_update(NUMCOMMENTS, key, comment, USERNAME):
-    print(create_message(NUMCOMMENTS, key, comment))
-    subject = "Someone is talking about " + key
-    r.send_message(USERNAME, subject, create_message(NUMCOMMENTS, key, comment))
+def send_update(key, comment, USERNAME):
+    print(create_message(comment))
+    subject = "Someone is talking about " + key + "!"
+    r.send_message(USERNAME, subject, create_message(key, comment))
     print("Message Sent to " + USERNAME)
 
-
+commDone = set()
+subDone = set()
 r = obot.login()
 print("Logged in")
-print(strftime("%Y-%m-%d %H:%M:%S") + " Searching for: " + str(SETPHRASES))
+print(strftime("%Y-%m-%d %H:%M:%S") + " Searching for: " + str(KEYPHRASES))
 
 while True:
     try:
@@ -36,14 +33,16 @@ while True:
             if comment.id not in commDone:
                 commDone.add(comment.id)
                 for word in comment.body.lower().split():
-                    for key in SETPHRASES:
+                    for key in KEYPHRASES:
                         if word == key:
-                            NUMCOMMENTS += 1
-                            send_update(NUMCOMMENTS, key, comment, USERNAME)
-                            if comment.submission.id not in subDone and comment.subreddit.display_name not in SUBBLACKLIST:
-                                comment.reply(COMMREPLY)
-                                print("Replied to comment")
-                                subDone.add(comment.submission.id)
+                            send_update(key, comment, USERNAME)
+                            if comment.subreddit.display_name not in SUBBLACKLIST:
+                                if comment.submission.id not in subDone:
+                                    comment.reply(COMMREPLY)
+                                    print("Replied to comment")
+                                    subDone.add(comment.submission.id)
+                                else:
+                                    print("Reply not sent, submission already replied to.")
                             else:
                                 print("Reply not sent, subreddit black-listed.")
                                 subDone.add(comment.submission.id)
