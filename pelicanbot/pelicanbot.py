e1138759bfda3cbcb413294a862d09cbc34865f6
SecretAg3nt <hawkinsliam@hotmail.com>
2015-11-10 07:17:53 -0500

    made function send_message, broke NUMCOMMENTS counter

--- pelicanbot/pelicanbot.py
+++ pelicanbot/pelicanbot.py
@@ -5,7 +5,7 @@ from time import strftime
 
 NUMCOMMENTS = 0
 SETPHRASES = ["pelican", "pelicans"]
-SUBBLACKLIST = ["nba"]
+SUBBLACKLIST = ["nba", "test"]
 COMMLIMIT = 100
 COMMREPLY = "Don't fuck with the [pelicans](https://www.youtube.com/watch?v=jWxIOdt-V8Y)"
 USERNAME = "SecretAg3nt"
@@ -17,6 +17,13 @@ subDone = set()
 # Creates msg from keyword and comment passed in
 def create_message(NUMCOMMENTS, key, comment):
     return str(NUMCOMMENTS) + ") " + strftime("%Y-%m-%d %H:%M:%S") + " Someone is talking about \"" + key + "\": " + comment.author.name + " " + comment.permalink
+
+
+def send_update(NUMCOMMENTS, key, comment, USERNAME):
+    print(create_message(NUMCOMMENTS, key, comment))
+    subject = "Someone is talking about " + key
+    r.send_message(USERNAME, subject, create_message(NUMCOMMENTS, key, comment))
+    print("Message Sent to " + USERNAME)
 
 
 r = obot.login()
@@ -31,13 +38,9 @@ while True:
                 for word in comment.body.lower().split():
                     for key in SETPHRASES:
                         if word == key:
-                            NUMCOMMENTS = NUMCOMMENTS + 1
-                            print(create_message(NUMCOMMENTS, key, comment))
-                            subject = "Someone is talking about " + key
-                            r.send_message(USERNAME, subject, create_message(NUMCOMMENTS, key, comment))
-                            print("Message Sent to " + USERNAME)
-                            
-                            if comment.submission.id not in subDone and not comment.subreddit.display_name in SUBBLACKLIST:
+                            NUMCOMMENTS += NUMCOMMENTS
+                            send_update(NUMCOMMENTS, key, comment, USERNAME)
+                            if comment.submission.id not in subDone and comment.subreddit.display_name not in SUBBLACKLIST:
                                 comment.reply(COMMREPLY)
                                 print("Replied to comment")
                                 subDone.add(comment.submission.id)
