537279977735f7d789073f7c4ff19ce75801b2c9
SecretAg3nt <hawkinsliam@hotmail.com>
2015-11-11 12:31:14 -0500

    NUMCOMMENTS throwing an out of scope error, among other things, might still be broken

--- pelicanbot/pelicanbot.py
+++ pelicanbot/pelicanbot.py
@@ -3,39 +3,39 @@ import obot
 from time import strftime
 
 
-KEYPHRASES = ["pelican", "pelicans"]
-SUBBLACKLIST = ["nba", "test"]
-COMMLIMIT = 100
-COMMREPLY = "Don't fuck with the [pelicans](https://www.youtube.com/watch?v=jWxIOdt-V8Y)"
-USERNAME = "SecretAg3nt"
-NUMCOMMENT = 1
-
-commDone = set()
-subDone = set()
-r = obot.login()
-
-
 # Creates msg from keyword and comment passed in
 def create_message(comment):
     return "PelicanBot found someone talking about pelicans! " + comment.author.name + " " + comment.permalink
 
 
 def terminal_prompt(comment, NUMCOMMENT):
-    return NUMCOMMENT + ") " + comment.author.name + " - r/" + comment.subreddit.display_name + "\n"
+    return str(NUMCOMMENT) + ") " + comment.author.name + " - r/" + comment.subreddit.display_name + "\n"
 
 
-def send_update(key, comment, USERNAME, NUMCOMMENT):
+def send_update(r, key, comment, USERNAME, NUMCOMMENT):
     print(terminal_prompt(comment, NUMCOMMENT))
     subject = "Someone is talking about " + key + "!"
-    r.send_message(USERNAME, subject, create_message(key, comment))
+    r.send_message(USERNAME, subject, create_message(comment))
 
 
 def run():
+
+    KEYPHRASES = ["pelican", "pelicans"]
+    SUBBLACKLIST = ["nba"]
+    COMMLIMIT = 100
+    COMMREPLY = "Don't fuck with the [pelicans](https://www.youtube.com/watch?v=jWxIOdt-V8Y)"
+    USERNAME = "SecretAg3nt"
+    NUMCOMMENT = 0
+
+    commDone = set()
+    subDone = set()
+    r = obot.login()
+
     print("Logged in")
     print(strftime("%Y-%m-%d %H:%M:%S") + " Searching for: " + str(KEYPHRASES))
 
     while True:
-        try:
+       # try:
             for comment in praw.helpers.comment_stream(r, 'all', limit=COMMLIMIT, verbosity=0):
                 if comment.id not in commDone:
                     commDone.add(comment.id)
@@ -44,23 +44,20 @@ def run():
                             if word == key:
                                 if comment.subreddit.display_name not in SUBBLACKLIST:
                                     if comment.submission.id not in subDone:
-                                        comment.reply(COMMREPLY)
-                                        send_update(key, comment, USERNAME, NUMCOMMENT)
-                                        print("Replied to comment")
-                                        subDone.add(comment.submission.id)
                                         NUMCOMMENT += 1
+                                        comment.reply(COMMREPLY)
+                                        send_update(r, key, comment, USERNAME, NUMCOMMENT)
+                                        subDone.add(comment.submission.id)
+
                                     else:
                                         print("Reply not sent, submission already replied to. " + comment.permalink + "\n")
                                 else:
-                                    print("Reply not sent, subreddit black-listed. " + comment.permalink + "\n")
+                                    print("Reply not sent, subreddit black-listed. " + comment.subreddit.display_name + "\n")
                                     subDone.add(comment.submission.id)
-                                    break
-                        else:
-                            continue
-                        break
 
-        except:
-            pass
+
+      #  except:
+           # pass
 
 if __name__ == "__main__":
     run()
