77f7720f4e3667d7c481998ed83345d5b1cd454c
SecretAg3nt <hawkinsliam@hotmail.com>
2015-11-11 11:08:45 -0500

    Added function terminal_prompt to have different message in terminal when a comment is found from the one that is send in the reddit msg

--- pelicanbot/pelicanbot.py
+++ pelicanbot/pelicanbot.py
@@ -8,6 +8,7 @@ SUBBLACKLIST = ["nba", "test"]
 COMMLIMIT = 100
 COMMREPLY = "Don't fuck with the [pelicans](https://www.youtube.com/watch?v=jWxIOdt-V8Y)"
 USERNAME = "SecretAg3nt"
+NUMCOMMENT = 1
 
 commDone = set()
 subDone = set()
@@ -19,11 +20,14 @@ def create_message(comment):
     return "PelicanBot found someone talking about pelicans! " + comment.author.name + " " + comment.permalink
 
 
-def send_update(key, comment, USERNAME):
-    print(create_message(comment) + "\n")
+def terminal_prompt(comment, NUMCOMMENT):
+    return NUMCOMMENT + ") " + comment.author.name + " - r/" + comment.subreddit.display_name + "\n"
+
+
+def send_update(key, comment, USERNAME, NUMCOMMENT):
+    print(terminal_prompt(comment, NUMCOMMENT))
     subject = "Someone is talking about " + key + "!"
     r.send_message(USERNAME, subject, create_message(key, comment))
-    print("Message Sent to " + USERNAME)
 
 
 def run():
@@ -41,9 +45,10 @@ def run():
                                 if comment.subreddit.display_name not in SUBBLACKLIST:
                                     if comment.submission.id not in subDone:
                                         comment.reply(COMMREPLY)
-                                        send_update(key, comment, USERNAME)
+                                        send_update(key, comment, USERNAME, NUMCOMMENT)
                                         print("Replied to comment")
                                         subDone.add(comment.submission.id)
+                                        NUMCOMMENT += 1
                                     else:
                                         print("Reply not sent, submission already replied to. " + comment.permalink + "\n")
                                 else:
