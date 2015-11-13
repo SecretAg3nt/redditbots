8f8a2db1e077515f44cef23e1c2ad72c3cd56cd5
SecretAg3nt <hawkinsliam@hotmail.com>
2015-11-12 20:32:00 -0500

    refactored as a class

--- pelicanbot/pelicanbot.py
+++ pelicanbot/pelicanbot.py
@@ -4,48 +4,49 @@ from time import strftime
 import os
 
 
-def terminal_prompt(comment, NUMCOMMENT):
-    print(str(NUMCOMMENT) + ") " + comment.author.name + " - r/" + comment.subreddit.display_name + "\n")
+class pelicanBot(object):
 
+    def __init__(self):
+        self.KEYPHRASES = ["pelican", "pelicans"]
+        self.SUBBLACKLIST = ["nba"]
+        self.COMMLIMIT = 100
+        self.COMMREPLY = "Don't fuck with the [pelicans](https://www.youtube.com/watch?v=jWxIOdt-V8Y)"
+        self.NUMCOMMENT = 0
+        self.commDone = set()
+        self.subDone = set()
+        self.r = obot.login()
 
-def run():
+    def terminal_prompt(self, comment, NUMCOMMENT):
+        print(str(NUMCOMMENT) + ") " + comment.author.name + " - r/" + comment.subreddit.display_name + "\n")
 
-    KEYPHRASES = ["pelican", "pelicans"]
-    SUBBLACKLIST = ["nba"]
-    COMMLIMIT = 100
-    COMMREPLY = "Don't fuck with the [pelicans](https://www.youtube.com/watch?v=jWxIOdt-V8Y)"
-    NUMCOMMENT = 0
+    def run(self):
+        print("Logged in")
+        print(strftime("%Y-%m-%d %H:%M:%S") + " Searching for: " + str(self.KEYPHRASES))
 
-    commDone = set()
-    subDone = set()
-    r = obot.login()
-
-    print("Logged in")
-    print(strftime("%Y-%m-%d %H:%M:%S") + " Searching for: " + str(KEYPHRASES))
-
-    while True:
-        try:
-            for comment in praw.helpers.comment_stream(r, 'all', limit=COMMLIMIT, verbosity=1):
-                if comment.id not in commDone:
-                    commDone.add(comment.id)
-                    for word in comment.body.lower().split():
-                        for key in KEYPHRASES:
-                            if word == key:
-                                if comment.subreddit.display_name not in SUBBLACKLIST:
-                                    if comment.submission.id not in subDone:
-                                        NUMCOMMENT += 1
-                                        comment.reply(COMMREPLY)
-                                        subDone.add(comment.submission.id)
-                                        terminal_prompt(comment, NUMCOMMENT)
+        while True:
+            try:
+                for comment in praw.helpers.comment_stream(self.r, 'all', limit=self.COMMLIMIT, verbosity=1):
+                    if comment.id not in self.commDone:
+                        self.commDone.add(comment.id)
+                        for word in comment.body.lower().split():
+                            for key in self.KEYPHRASES:
+                                if word == key:
+                                    if comment.subreddit.display_name not in self.SUBBLACKLIST:
+                                        if comment.submission.id not in self.subDone:
+                                            self.NUMCOMMENT += 1
+                                            comment.reply(self.COMMREPLY)
+                                            self.subDone.add(comment.submission.id)
+                                            self.terminal_prompt(comment, self.NUMCOMMENT)
+                                        else:
+                                            print("Reply not sent, submission already replied to. " + "\n")
                                     else:
-                                        print("Reply not sent, submission already replied to. " + "\n")
-                                else:
-                                    print("Reply not sent, subreddit black-listed. " + comment.subreddit.display_name + "\n")
-                                    subDone.add(comment.submission.id)
+                                        print("Reply not sent, subreddit black-listed. r/" + comment.subreddit.display_name + "\n")
+                                        self.subDone.add(comment.submission.id)
 
-        except:
-            pass
+            except:
+                pass
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
-    run()
+    pelbot = pelicanBot()
+    pelbot.run()
